fc8dd08c78de4cf35360ac9000d0e524d95be9f4
Benjamin York <benjaminyork@gmail.com>
2014-08-12 16:27:13 -0500

    Added iterator and rearranged functions.

--- Network.js
+++ Network.js
@@ -1,5 +1,39 @@
 "use strict";
 
+/***** NetworkIterator *****/
+
+function NetworkIterator(start, end) {
+	start = Network.prototype.ipStringToInteger(start);
+	end = Network.prototype.ipStringToInteger(end);
+
+	if (start > end) {
+		throw new RangeError('Start is after end.');
+	}
+
+	this._start = start;
+	this._end = end;
+	this._current = start;
+}
+
+NetworkIterator.prototype.next = function() {
+	return (this._current <= this._end) ? Network.prototype.ipIntegerToString(this._current++) : null;
+};
+
+NetworkIterator.prototype.reset = function() {
+	this._current = this._start;
+};
+
+NetworkIterator.prototype.toArray = function() {
+	var result, ip;
+
+	result = [];
+	while (ip = this.next()) {
+		result.push(ip);
+	}
+
+	return result;
+};
+
 /***** CONSTRUCTOR *****/
 
 function Network(ipString, submaskString) {
@@ -37,7 +71,7 @@ function Network(ipString, submaskString) {
 		this._submask = this.ipStringToInteger(submaskString);
 
 		if (!this.contains(result.start, result.end)) {
-			throw new Error('Invalid range/submask combination.');
+			throw new RangeError('Invalid range/submask combination.');
 		}
 
 		// Example Match: 192.168.0.25-192.168.5.32, 255.255.0.0
@@ -49,7 +83,7 @@ function Network(ipString, submaskString) {
 		this._submask = this.ipStringToInteger(submaskString);
 
 		if (!this.contains(result.start, result.end)) {
-			throw new Error('Invalid range/submask combination.');
+			throw new RangeError('Invalid range/submask combination.');
 		}
 
 		// Bad input.
@@ -60,25 +94,48 @@ function Network(ipString, submaskString) {
 
 /***** STATICS VARIABLES *****/
 
-// Example Match: 192.168.1.5
-Network.prototype._ipRegex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-
-// Example Match: 255.255.255.0
-Network.prototype._submaskRegex = /^(?:255\.){3}(?:255|254|252|248|240|224|192|128|0)|(?:255\.){2}(?:254|252|248|240|224|192|128|0)\.0|255\.(?:254|252|248|240|224|192|128|0)(?:\.0){2}|(?:254|252|248|240|224|192|128)(?:\.0){3}$/;
-
 // Example Match: 192.168.1.0/24
 Network.prototype._cidrRegex = /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\/([1-2]?[0-9]|3[0-2])$/;
 
-// Example Match: 192.168.1.25-30
-Network.prototype._shortRangeRegex = /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3})(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)-(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+// Example Match: 192.168.1.5
+Network.prototype._ipRegex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
 // Example Match: 192.168.0.25-192.168.5.32
 Network.prototype._longRangeRegex = /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))-((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))$/;
 
+// Example Match: 192.168.1.25-30
+Network.prototype._shortRangeRegex = /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3})(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)-(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+// Example Match: 255.255.255.0
+Network.prototype._submaskRegex = /^(?:255\.){3}(?:255|254|252|248|240|224|192|128|0)|(?:255\.){2}(?:254|252|248|240|224|192|128|0)\.0|255\.(?:254|252|248|240|224|192|128|0)(?:\.0){2}|(?:254|252|248|240|224|192|128)(?:\.0){3}$/;
+
 /***** STATICS FUNCTIONS *****/
+
+Network.prototype._bitmaskStringToInteger = function(bitmask) {
+	var binary, remove, index;
+
+	bitmask = parseInt(bitmask, 10);
+
+	if (isNaN(bitmask) || bitmask > 32) {
+		throw new Error('Invalid bitmask.');
+	}
+
+	remove = 32 - bitmask;
+	binary = 0xffffffff;
+
+	for (index = 0; index < remove; index += 1) {
+		binary = binary ^ (0x1 << index);
+	}
+
+	return binary;
+};
 
 Network.prototype.bitAt = function(binary, num) {
 	return ((binary & (1 << (num - 1))) !== 0) ? 1 : 0;
+};
+
+Network.prototype.bitmaskToSubmask = function(bitmask) {
+	return Network.prototype.ipIntegerToString(Network.prototype._bitmaskStringToInteger(bitmask));
 };
 
 Network.prototype.calculateSmallestSubmask = function(start, end) {
@@ -108,30 +165,64 @@ Network.prototype.getOctets = function(ipString) {
 	});
 };
 
-Network.prototype.isIp = function(ipString) {
-	return Network.prototype._ipRegex.test(ipString);
+Network.prototype.ipIntegerToString = function(ipInteger) {
+	var octets, index, shift;
+
+	// Get decimal of each octet.
+	octets = [];
+	for (index = 0; index < 4; index += 1) {
+		shift = (3 - index) * 8;
+		octets.push((ipInteger & (0xff << shift)) >>> shift);
+	}
+
+	// Join decimal octets into string and return.
+	return octets.join('.');
 };
 
-Network.prototype.isSubmask = function(submaskString) {
-	return Network.prototype._submaskRegex.test(submaskString);
+Network.prototype.ipStringToInteger = function(ipString) {
+	var octets, result, index, shift;
+
+	if (!Network.prototype.isIp(ipString)) {
+		throw new Error('Invalid IP.');
+	}
+
+	// Get octet int values.
+	octets = Network.prototype.getOctets(ipString);
+
+	// Sum binary octets.
+	result = 0;
+	for (index = 0; index < 4; index += 1) {
+		shift = (3 - index) * 8;
+		result += (octets[index] << shift);
+	}
+
+	return result;
 };
 
 Network.prototype.isCidr = function(cidrString) {
 	return Network.prototype._cidrRegex.test(cidrString);
 };
 
-Network.prototype.isShortRange = function(shortRangeString) {
-	return Network.prototype._shortRangeRegex.test(shortRangeString);
+Network.prototype.isIp = function(ipString) {
+	return Network.prototype._ipRegex.test(ipString);
 };
 
 Network.prototype.isLongRange = function(longRangeString) {
 	return Network.prototype._longRangeRegex.test(longRangeString);
 };
 
+Network.prototype.isShortRange = function(shortRangeString) {
+	return Network.prototype._shortRangeRegex.test(shortRangeString);
+};
+
+Network.prototype.isSubmask = function(submaskString) {
+	return Network.prototype._submaskRegex.test(submaskString);
+};
+
 Network.prototype.parseCidr = function(cidrString) {
 	var result, regexResult;
 
-	result = undefined;
+	result = null;
 	regexResult = Network.prototype._cidrRegex.exec(cidrString);
 
 	if (regexResult) {
@@ -144,35 +235,10 @@ Network.prototype.parseCidr = function(cidrString) {
 	return result;
 };
 
-Network.prototype.parseShortRange = function(shortRangeString) {
-	var result, regexResult, start, end, swap;
-
-	result = undefined;
-	regexResult = Network.prototype._shortRangeRegex.exec(shortRangeString);
-
-	if (regexResult) {
-		start = Network.prototype.ipStringToInteger(regexResult[1] + regexResult[2]);
-		end = Network.prototype.ipStringToInteger(regexResult[1] + regexResult[3]);
-
-		if (start > end) {
-			swap = start;
-			start = end;
-			end = swap;
-		}
-
-		result = {
-			start: Network.prototype.ipIntegerToString(start),
-			end: Network.prototype.ipIntegerToString(end)
-		};
-	}
-
-	return result;
-};
-
 Network.prototype.parseLongRange = function(longRangeString) {
 	var result, regexResult, start, end, swap;
 
-	result = undefined;
+	result = null;
 	regexResult = Network.prototype._longRangeRegex.exec(longRangeString);
 
 	if (regexResult) {
@@ -194,86 +260,32 @@ Network.prototype.parseLongRange = function(longRangeString) {
 	return result;
 };
 
-Network.prototype._bitmaskStringToInteger = function(bitmask) {
-	var binary, remove, index;
+Network.prototype.parseShortRange = function(shortRangeString) {
+	var result, regexResult, start, end, swap;
 
-	bitmask = parseInt(bitmask, 10);
+	result = null;
+	regexResult = Network.prototype._shortRangeRegex.exec(shortRangeString);
 
-	if (isNaN(bitmask) || bitmask > 32) {
-		throw new Error('Invalid bitmask.');
-	}
+	if (regexResult) {
+		start = Network.prototype.ipStringToInteger(regexResult[1] + regexResult[2]);
+		end = Network.prototype.ipStringToInteger(regexResult[1] + regexResult[3]);
 
-	remove = 32 - bitmask;
-	binary = 0xffffffff;
+		if (start > end) {
+			swap = start;
+			start = end;
+			end = swap;
+		}
 
-	for (index = 0; index < remove; index += 1) {
-		binary = binary ^ (0x1 << index);
-	}
-
-	return binary;
-};
-
-Network.prototype.bitmaskToSubmask = function(bitmask) {
-	return Network.prototype.ipIntegerToString(Network.prototype._bitmaskStringToInteger(bitmask));
-};
-
-Network.prototype.ipStringToInteger = function(ipString) {
-	var octets, result, index, shift;
-
-	if (!Network.prototype.isIp(ipString)) {
-		throw new Error('Invalid IP.');
-	}
-
-	// Get octet int values.
-	octets = Network.prototype.getOctets(ipString);
-
-	// Sum binary octets.
-	result = 0;
-	for (index = 0; index < 4; index += 1) {
-		shift = (3 - index) * 8;
-		result += (octets[index] << shift);
+		result = {
+			start: Network.prototype.ipIntegerToString(start),
+			end: Network.prototype.ipIntegerToString(end)
+		};
 	}
 
 	return result;
-
-};
-
-Network.prototype.ipIntegerToString = function(ipInteger) {
-	var octets, index, shift;
-
-	// Get decimal of each octet.
-	octets = [];
-	for (index = 0; index < 4; index += 1) {
-		shift = (3 - index) * 8;
-		octets.push((ipInteger & (0xff << shift)) >>> shift);
-	}
-
-	// Join decimal octets into string and return.
-	return octets.join('.');
 };
 
 /***** METHODS *****/
-
-Network.prototype._calculateNetwork = function() {
-	if (!this._network) {
-		this._network = this._ip & this._submask;
-	}
-};
-
-Network.prototype.getNetwork = function() {
-	this._calculateNetwork();
-	return this.ipIntegerToString(this._network);
-};
-
-Network.prototype.getSubmask = function() {
-	return this.ipIntegerToString(this._submask);
-};
-
-Network.prototype._calculateHost = function() {
-	if (!this._host) {
-		this._host = this._submask ^ 0xffffffff;
-	}
-};
 
 Network.prototype._calculateBroadcast = function() {
 	if (!this._broadcast) {
@@ -283,24 +295,6 @@ Network.prototype._calculateBroadcast = function() {
 	}
 };
 
-Network.prototype.getBroadcast = function() {
-	this._calculateBroadcast();
-	return this.ipIntegerToString(this._broadcast);
-
-};
-
-Network.prototype._calculateStart = function() {
-	if (!this._start) {
-		this._calculateNetwork();
-		this._start = this._network + 1;
-	}
-};
-
-Network.prototype.getStart = function() {
-	this._calculateStart();
-	return this.ipIntegerToString(this._start);
-};
-
 Network.prototype._calculateEnd = function() {
 	if (!this._end) {
 		this._calculateBroadcast();
@@ -308,23 +302,23 @@ Network.prototype._calculateEnd = function() {
 	}
 };
 
-Network.prototype.getEnd = function() {
-	this._calculateEnd();
-	return this.ipIntegerToString(this._end);
+Network.prototype._calculateHost = function() {
+	if (!this._host) {
+		this._host = this._submask ^ 0xffffffff;
+	}
 };
 
-Network.prototype.getRange = function() {
-	var range, currentIp;
-
-	this._calculateStart();
-	this._calculateEnd();
-
-	range = [];
-	for (currentIp = this._start; currentIp <= this._end; currentIp += 1) {
-		range.push(this.ipIntegerToString(currentIp));
+Network.prototype._calculateNetwork = function() {
+	if (!this._network) {
+		this._network = this._ip & this._submask;
 	}
+};
 
-	return range;
+Network.prototype._calculateStart = function() {
+	if (!this._start) {
+		this._calculateNetwork();
+		this._start = this._network + 1;
+	}
 };
 
 Network.prototype._contains = function(ip) {
@@ -368,6 +362,52 @@ Network.prototype.contains = function(start, end) {
 	}
 };
 
+Network.prototype.createIterator = function(start, end) {
+	start = start || Network.prototype.ipIntegerToString(this._start);
+	end = end || Network.prototype.ipIntegerToString(this._end);
+
+	if (!this.contains(start)) {
+		throw new RangeError('Network does not contain start IP address.');
+	}
+
+	if (!this.contains(end)) {
+		throw new RangeError('Network does not contain end IP address.');
+	}
+
+	return new NetworkIterator(start, end);
+};
+
+Network.prototype.getBroadcast = function() {
+	this._calculateBroadcast();
+	return this.ipIntegerToString(this._broadcast);
+
+};
+
+Network.prototype.getEnd = function() {
+	this._calculateEnd();
+	return this.ipIntegerToString(this._end);
+};
+
+Network.prototype.getNetwork = function() {
+	this._calculateNetwork();
+	return this.ipIntegerToString(this._network);
+};
+
+Network.prototype.getRange = function() {
+	this._calculateStart();
+	this._calculateEnd();
+	return this.createIterator().toArray();
+};
+
+Network.prototype.getStart = function() {
+	this._calculateStart();
+	return this.ipIntegerToString(this._start);
+};
+
+Network.prototype.getSubmask = function() {
+	return this.ipIntegerToString(this._submask);
+};
+
 Network.prototype.serialize = function() {
 	return {
 		network: this.getNetwork(),
@@ -376,6 +416,10 @@ Network.prototype.serialize = function() {
 		start: this.getStart(),
 		end: this.getEnd()
 	}
+};
+
+Network.prototype.toString = function() {
+	return JSON.stringify(this.serialize());
 };
 
 module.exports = Network;
