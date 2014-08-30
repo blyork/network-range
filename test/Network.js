dfb73e5061800d527131cfccb94303746a44a054
Benjamin York <benjaminyork@gmail.com>
2014-08-29 21:07:19 -0500

    Added some mocha tests.

--- test/Network.js
+++ test/Network.js
@@ -4,18 +4,33 @@ describe('Network', function() {
 	var expect, Network;
 
 	expect = require('chai').expect;
-	Network = require('./Network');
+	Network = require('../development/Network.js');
 
 	describe('static functions', function() {
 		describe('#bitAt', function() {
-			it('should ', function() {
-
+			it('should return bit at position (right to left) in number', function() {
+				var index, binary, num;
+				for (index; index < 100; index += 1) {
+					binary = Math.floor(Math.random() * 4294967295);
+					num = Math.floor(Math.random() * 32);
+					expect(Network.prototype.bitAt(binary, num)).to.equal(binary.toString(2)[33 - num]);
+				}
 			});
 		});
 
 		describe('#bitmaskToSubmask', function() {
-			it('should ', function() {
-
+			it('should return string submask base on number bitmask', function() {
+				expect(Network.prototype.bitmaskToSubmask(24)).to.equal('255.255.255.0');
+				expect(Network.prototype.bitmaskToSubmask(16)).to.equal('255.255.0.0');
+				expect(Network.prototype.bitmaskToSubmask(8)).to.equal('255.0.0.0');
+				expect(Network.prototype.bitmaskToSubmask(31)).to.equal('255.255.255.254');
+				expect(Network.prototype.bitmaskToSubmask(5)).to.equal('248.0.0.0');
+				expect(Network.prototype.bitmaskToSubmask(2)).to.equal('192.0.0.0');
+				expect(Network.prototype.bitmaskToSubmask(22)).to.equal('255.255.252.0');
+				expect(Network.prototype.bitmaskToSubmask(15)).to.equal('255.254.0.0');
+				expect(Network.prototype.bitmaskToSubmask(30)).to.equal('255.255.255.252');
+				expect(Network.prototype.bitmaskToSubmask(8)).to.equal('255.0.0.0');
+				expect(Network.prototype.bitmaskToSubmask(18)).to.equal('255.255.192.0');
 			});
 		});
 
@@ -26,8 +41,17 @@ describe('Network', function() {
 		});
 
 		describe('#getOctets', function() {
-			it('should ', function() {
-
+			it('should return an array of integer based octets', function() {
+				var index, output;
+				for (index = 0; index < 100; index += 1) {
+					output = [
+						Math.floor(Math.random() * 255),
+						Math.floor(Math.random() * 255),
+						Math.floor(Math.random() * 255),
+						Math.floor(Math.random() * 255)
+					];
+					expect(Network.prototype.getOctets(output.join('.'))).to.deep.equal(output);
+				}
 			});
 		});
 
